8e767ba428e63056c3dc009456e9045042eef916
Данил <@DESKTOP-QGKFTF2>
2022-12-25 19:38:31 +0300

    Добавьте файлы проекта.

---
+++ MultiplesNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿using MultiplesNumbers;
+Numbers number = new();
+Console.WriteLine("Введите число");
+Console.WriteLine();
+string enter = Console.ReadLine();
+bool isParse = int.TryParse(enter, out var numbers);
+if (isParse)
+{
+    number.CheckingForTheMultiplicityOfANumber(numbers);
+}
+else
+{
+    Console.WriteLine();
+    Console.WriteLine("Ошибка");
+    Console.WriteLine();
+}
